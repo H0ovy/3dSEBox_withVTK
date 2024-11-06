97ac342c1550031821a14c0e8d159e66b0c0abaa
Vladislav-fesc <145829645+Vladislav-fesc@users.noreply.github.com>
2024-11-06 17:42:05 +0700

    Add files via upload

--- mainwindow.h
+++ mainwindow.h
@@ -1,15 +1,20 @@
+﻿// mainwindow.h
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QMainWindow>
+#include <vtkNamedColors.h>
+#include <vtkSmartPointer.h>
+#include <vtkCylinderSource.h>
+#include <vtkCubeSource.h>.h>
 #include <QtCharts/QChartView>
 #include <QtCharts/QLineSeries>
 #include <QtCharts/QValueAxis>
-#include <QMainWindow>
+#include <vtkActor.h>
+
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -22,13 +27,33 @@ public:
 
 private slots:
     void on_pushButton_2D_clicked();
-
     void on_pushButton_3D_clicked();
+
+    void on_pushButton_Figure_3_clicked();
+
+    void on_pushButton_Figure_1_clicked();
+
+    void on_pushButton_Figure_2_clicked();
+
+    void on_lineEdit_size_a_selectionChanged();
+
+    void on_lineEdit_size_b_selectionChanged();
+
+    void on_lineEdit_size_d_selectionChanged();
 
 private:
     Ui::MainWindow *ui;
+    vtkSmartPointer<vtkCylinderSource> cylinderSource;
+    vtkSmartPointer<vtkCubeSource> rectangleSource;
+    vtkSmartPointer<vtkActor> cylinderActor;
+
+    void updateCylinder(double radius, double height);
+    bool graph_2d_exists = false;
     QLineSeries *series;
     QChartView *chartView;
-    bool graph_2d_exists = false;
+    int figure;
+
+    vtkNew<vtkNamedColors> colors;
 };
+
 #endif // MAINWINDOW_H
